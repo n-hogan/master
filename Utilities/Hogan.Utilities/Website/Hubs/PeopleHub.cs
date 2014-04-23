8443e19981f1a6d16446911e18292f41a06ea2b9
decoderdan <daniel5.davies@live.uwe.ac.uk>
2014-04-23 15:21:41 +0100

    server side push

--- Utilities/Hogan.Utilities/Website/Hubs/PeopleHub.cs
+++ Utilities/Hogan.Utilities/Website/Hubs/PeopleHub.cs
@@ -1,14 +1,28 @@
-﻿using Hcs.Data.Entities;
+﻿using System.Security.Cryptography.X509Certificates;
+using Hcs.Data.Entities;
 using Hcs.Data.Repository;
+using Microsoft.AspNet.SignalR;
 
 namespace Website.Hubs
 {
     public class PeopleHub : DataHubBase<Person>
     {
+        
+
         public PeopleHub(IRepository<Person> repo) : base(repo)
         {
             
         }
+
+        public void Push()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<PeopleHub>();
+            context.Clients.All.addPersonToPage(4, "Alex");
+        }
+
+
+
+
     }
 }
 
